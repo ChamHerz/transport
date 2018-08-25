5f4d822da625376fbd6200caa22e9718c38acf92
ChamHerz <adrielchambers@gmail.com>
2018-08-25 19:48:57 -0300

    last push

--- src/main/java/forms/FormPrincipal.java
+++ src/main/java/forms/FormPrincipal.java
@@ -4,6 +4,8 @@ import controles.BarraHerramientas;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.net.URL;
 
 public class FormPrincipal extends JFrame {
@@ -13,8 +15,10 @@ public class FormPrincipal extends JFrame {
 
     public FormPrincipal() throws HeadlessException {
         this.setTitle("Transport");
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setExtendedState(MAXIMIZED_BOTH);
+
+        this.addWindowListener( new SalirSistema() );
 
         desktopPanel = new JDesktopPane();
         this.getContentPane().add(desktopPanel);
@@ -22,6 +26,13 @@ public class FormPrincipal extends JFrame {
         BarraHerramientas barraHerramientas = new BarraHerramientas();
         this.getContentPane().add(barraHerramientas, BorderLayout.NORTH);
 
+        //barraHerramientas.addAncestorListener();
 
+    }
+}
+
+class SalirSistema extends WindowAdapter {
+    public void windowClosing( WindowEvent evt ) {
+        System.exit( 0 );
     }
 }
